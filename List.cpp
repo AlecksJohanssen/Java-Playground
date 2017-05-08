5173156af31b4a36ab59505853d6d4e6d17d0de6
AlecksJohanssen <minh.dq1078@sinhvien.hoasen.edu.vn>
2017-05-08 20:21:58 +0700

    More about list and add new file extension to gitignore

--- List.cpp
+++ List.cpp
@@ -1,21 +1,29 @@
 #include <stdio.h>
-
+#include <iostream>
+using namespace std;
 
 class List {
   struct Node {
     int data;
     Node * next;
   };
+  int count;
   Node * head;
   public:
   List() {
     head = NULL;
   }
   ~List() {
+    printf("%d current data", head -> data);
     while(head != NULL) {
       Node * n = head -> next;
       delete head;
       head = n;
+      if( head == NULL ) {
+        printf("Head Null");
+      } else {
+        printf("%d \n", head-> data);
+      }
     }
   }
   void add(int value) {
@@ -23,6 +31,7 @@ class List {
     n -> data = value;
     n -> next = head;
     head = n;
+    printf("Add First");
   }
 
   int size() {
@@ -34,11 +43,24 @@ class List {
     }
     return count;
   }
+  
+  void Print(Node* head)
+  {
+    while (head != NULL)
+    {
+      cout << head->data << endl;
+      head = head->next;
+    }
+    cout << "NULL";
+  }
 };
 
 int main() {
   List a;
   a.add(1);
-  printf("%d", a.size());
+  a.add(2000);
+  a.add(12);
+  a.add(100);
+  a.add(1999);
   return 0;
 }
